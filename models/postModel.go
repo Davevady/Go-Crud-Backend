05f15068730a220d2b7b709f8d86223712175568
Seyora24 <septianyogaprawira@gmail.com>
2023-07-06 10:19:54 +0700

    JWT Authorization

--- models/postModel.go
+++ models/postModel.go
@@ -1,13 +1,16 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type Post struct {
 	gorm.Model
-	Name    string
-	Email   string
-	Gender  string
-	Address string
-	Tempat  string
-	TLahir  string
+	Name     string
+	Email    string `gorm:"unique"`
+	Password string
+	Gender   string
+	Address  string
+	Tempat   string
+	TLahir   string
 }
